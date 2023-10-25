e8fba7b0da2b48a7afa810b3418e9ff427e8ffcc
Pol Gubau Amores <gubaupol@gmail.com>
2023-10-25 12:51:28 +0200

    Pol Gubau; MW-Auth server

--- public/scripts/Auth.js
+++ public/scripts/Auth.js
@@ -2,37 +2,58 @@ import API from "./API.js";
 import Router from "./Router.js";
 
 const Auth = {
-    isLoggedIn: false,
-    account: null,
-    updateStatus() {
-        if (Auth.isLoggedIn && Auth.account) {
-            document.querySelectorAll(".logged_out").forEach(
-                e => e.style.display = "none"
-            );
-            document.querySelectorAll(".logged_in").forEach(
-                e => e.style.display = "block"
-            );
-            document.querySelectorAll(".account_name").forEach(
-                e => e.innerHTML = Auth.account.name
-            );
-            document.querySelectorAll(".account_username").forEach(
-                e => e.innerHTML = Auth.account.email
-            );
-
-        } else {
-            document.querySelectorAll(".logged_out").forEach(
-                e => e.style.display = "block"
-            );
-            document.querySelectorAll(".logged_in").forEach(
-                e => e.style.display = "none"
-            );
-
-        }
-    },    
-    init: () => {
-        
-    },
-}
+  isLoggedIn: false,
+  account: null,
+  register: async (event) => {
+    event.preventDefault();
+    const user = {
+      username: document.getElementById("register_name").value,
+      password: document.getElementById("register_password").value,
+      email: document.getElementById("register_email").value,
+    };
+    const response = await API.register(user);
+    console.log(response);
+  },
+  login: async (event) => {
+    event.preventDefault();
+    const user = {
+      email: document.getElementById("login_email").value,
+      password: document.getElementById("login_password").value,
+    };
+    const response = await API.login(user);
+    console.log(response);
+    // if (response.ok) {
+    //   Auth.isLoggedIn = true;
+    //   Auth.account = response;
+    //   Auth.updateStatus();
+    //   Router.navigate("/dashboard");
+    // }
+  },
+  updateStatus() {
+    if (Auth.isLoggedIn && Auth.account) {
+      document
+        .querySelectorAll(".logged_out")
+        .forEach((e) => (e.style.display = "none"));
+      document
+        .querySelectorAll(".logged_in")
+        .forEach((e) => (e.style.display = "block"));
+      document
+        .querySelectorAll(".account_name")
+        .forEach((e) => (e.innerHTML = Auth.account.name));
+      document
+        .querySelectorAll(".account_username")
+        .forEach((e) => (e.innerHTML = Auth.account.email));
+    } else {
+      document
+        .querySelectorAll(".logged_out")
+        .forEach((e) => (e.style.display = "block"));
+      document
+        .querySelectorAll(".logged_in")
+        .forEach((e) => (e.style.display = "none"));
+    }
+  },
+  init: () => {},
+};
 Auth.updateStatus();
 
 export default Auth;
--- server.js
+++ server.js
@@ -12,9 +12,10 @@ const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 const app = express();
 app.use(express.json());
 
-const adapter = new JSONFile(__dirname + "/auth.json");
+const adapter = new JSONFile(__dirname + "/auth.json"); // database file
 const db = new Low(adapter);
 await db.read();
+
 db.data ||= { users: [] };
 
 const rpID = "localhost";
@@ -30,7 +31,75 @@ app.use(
   })
 );
 
+function findUser(email) {
+  const results = db.data.users.filter((user) => user.email === email);
+  return results.length > 0 ? results[0] : undefined;
+}
+
 // ADD HERE THE REST OF THE ENDPOINTS
+
+app.post("/auth/login", async (req, res) => {
+  // on body we'll have email and password
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400).send({ ok: false, message: "Missing fields" });
+    return;
+  }
+  if (password.length < 8) {
+    res.status(400).send({
+      ok: false,
+      message: "Password must be at least 8 characters long",
+    });
+    return;
+  }
+  const userFound = findUser(email);
+  if (!userFound) {
+    res.status(400).send({ ok: false, message: "User not found" });
+    return;
+  }
+  const isPasswordCorrect = bcrypt.compareSync(password, userFound.password);
+
+  if (!isPasswordCorrect) {
+    res.status(400).send({ ok: false, message: "Credentials are wrong" });
+    return;
+  }
+
+  res.send({ ok: true, name: userFound.username, email: userFound.email });
+});
+
+app.post("/auth/register", async (req, res) => {
+  const { username, password, email } = req.body;
+
+  if (!username || !password || !email) {
+    res.status(400).send({ ok: false, message: "Missing fields" });
+    return;
+  }
+
+  if (password.length < 8) {
+    res.status(400).send({
+      ok: false,
+      message: "Password must be at least 8 characters long",
+    });
+    return;
+  }
+
+  const userFound = findUser(email);
+  if (userFound) {
+    res.status(400).send({ ok: false, message: "Email already exists" });
+    return;
+  }
+
+  // user is new
+  const salt = bcrypt.genSaltSync(10);
+  const hashedPassword = bcrypt.hashSync(password, salt);
+  const newUser = {
+    username,
+    password: hashedPassword,
+    email,
+  };
+  db.data.users.push(newUser);
+  await db.write();
+});
 
 app.get("*", (req, res) => {
   res.sendFile(__dirname + "public/index.html");
